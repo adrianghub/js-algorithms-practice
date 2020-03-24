6f65eb64a8aacef9f99ab23ddd96dcb2128c0d91
Adrian <adrian_zinko@op.pl>
2020-03-24 14:50:59 +0100

    feat: change function name

--- reverse-it/script.js
+++ reverse-it/script.js
@@ -1,24 +1,16 @@
 const input = document.querySelector('.result__input');
 const result = document.querySelector('.result'); 
 
-function reverseMe(input) {
-
-    if(typeof input !== 'string') {
-  
-      throw new Error ("Ooops. Nie przekazałeś wartości typu  string!");
-      
-    } else {
-  
-      input.toLowerCase();
-      const reverseString = input.split('').reverse().join('');
+function reverseHandler(input) {
+ 
+      const lowInput = input.toLowerCase();
+      const reverseString = lowInput.split('').reverse().join('');
       return result.innerHTML = reverseString;
   
     }
-   
-  }
 
 document.forms[0].addEventListener('submit', (e) => {
   e.preventDefault();
 
-  reverseMe(input.value);
+  reverseHandler(input.value);
 })
