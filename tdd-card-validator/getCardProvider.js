e0de7ed6a5ab1fd4104b06823a6ff55417b99899
Adrian <adrian_zinko@op.pl>
2020-04-02 14:59:24 +0200

    feat: pass the tests for card validation module

--- tdd-card-validator/getCardProvider.js
+++ tdd-card-validator/getCardProvider.js
@@ -34,5 +34,33 @@ function normalize(cardNumber) {
 }
 
 function getProvider(cardNumber) {
-    throw new Error('Cannot recognize card provider');
+    const cardNumberString = cardNumber.toString();
+    if(isMasterCard(cardNumberString)) {  
+        return 'MasterCard'
+    } else if(isAmericanExpress(cardNumberString)) {
+        return 'American Express'
+    } else if(isVisa(cardNumberString)) {
+        return 'Visa'
+    } else {
+        throw new Error('Cannot recognize card provider');
+    }
+
+}
+
+function isMasterCard(cardNumberString) {
+    const isLength = cardNumberString.length === 16;
+    const isPrefix = ['51', '52', '53', '54', '55'].includes(cardNumberString.substring(0,2));
+    return isLength && isPrefix;
+}
+
+function isAmericanExpress(cardNumberString) {
+    const isLength = cardNumberString.length === 15;
+    const isPrefix = ['34', '37'].includes(cardNumberString.substring(0,2));
+    return isLength && isPrefix;
+}
+
+function isVisa(cardNumberString) {
+    const isLength = cardNumberString.length === 13 || cardNumberString.length === 16;
+    const isPrefix = cardNumberString.startsWith('4');
+    return isLength && isPrefix;
 }
