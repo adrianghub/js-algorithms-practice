eab679c3284db9d9ff098b1e0af07ed871d0160e
Adrian <adrian_zinko@op.pl>
2020-04-02 13:57:13 +0200

    feat: init test cases

--- tdd-card-validator/__tests__/getCardProvider.test.js
+++ tdd-card-validator/__tests__/getCardProvider.test.js
@@ -1,5 +1,49 @@
-// given 
-// when 
-// then
+const { getCardProvider } = require('../getCardProvider');
 
 
+test('should recognize MasterCard', () => {
+    // given
+    const cardNumber = 5555555555554444
+    // when 
+    const result = getCardProvider(cardNumber)
+    // then
+    expect(result).toBe('MasterCard')
+})
+
+test('should recognize American Express', () => {
+    // given
+     const cardNumber = 371449635398431
+     // when 
+     const result = getCardProvider(cardNumber)
+    // then
+    expect(result).toBe('American Express')
+})
+
+test('should recognize Visa', () => {
+    // given
+    const cardNumber = 4111111111111111
+   // when 
+   const result = getCardProvider(cardNumber)
+   // then
+   expect(result).toBe('Visa')
+})
+
+test('should mark number as incorrect', () => {
+    // given
+    const cardNumber = 123
+   // when 
+   const result = getCardProvider(cardNumber)
+   // then
+   expect(result).toBe('Incorrect number')
+})
+
+test('should throw if provider cannot be recognized', () => {
+    // given
+    const cardNumber = 3566002020360505
+   // when 
+   const result = getCardProvider(cardNumber)
+   // then
+   expect(() => {
+       getCardProvider(cardNumber)
+   }).toThrow('Cannot recognize card provider');
+})
